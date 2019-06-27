cf532b691fe73a4820a3742c63edcc38cf4ee24b
dan@digitalnode.net <dan@digitalnode.net>
2019-06-26 23:07:37 -0700

    Started building login tracker data aggregation table

--- classes/login_tracker.inc.php
+++ classes/login_tracker.inc.php
@@ -220,104 +220,100 @@ class LoginTracker{
 
 
 
-		?><form name="<?=$this->frm_name?>" id="<?=$this->frm_name?>" method="POST" action="<?=$_SERVER['REQUEST_URI']?>" onsubmit="loadLogins();return false">
-			<input type="hidden" name="searching_login">
+		?>
+		<form name="<?=$this->frm_name?>" id="<?=$this->frm_name?>" method="POST" action="<?=$_SERVER['REQUEST_URI']?>" onsubmit="loadLogins();return false">
+		
+		<input type="hidden" name="searching_login">
 
 		<table border="0" width="100%" class="lb" cellspacing="0">
 		<tr>
 			<td height="40" class="pad_left ui-widget-header">
-
 				<table border="0" width="100%" >
-				<tr>
-					<td width="500">
-						Login Tracker
-					</td>
+					<tr>
+						<td width="500">Login Tracker</td>
 
-					<td width="150" align="center">PAGE SIZE: <select name="<?=$this->order_prepend?>pagesizeDD" id="<?=$this->order_prepend?>pagesizeDD" onchange="<?=$this->index_name?>=0; loadLogins();return false">
-						<option value="20">20</option>
-						<option value="50">50</option>
-						<option value="100">100</option>
-						<option value="500">500</option>
-					</select></td>
+						<td width="150" align="center">PAGE SIZE: <select name="<?=$this->order_prepend?>pagesizeDD" id="<?=$this->order_prepend?>pagesizeDD" onchange="<?=$this->index_name?>=0; loadLogins();return false">
+							<option value="20">20</option>
+							<option value="50">50</option>
+							<option value="100">100</option>
+							<option value="500">500</option>
+						</select></td>
 
-					<td align="right"><?
-						/** PAGE SYSTEM CELLS -- INJECTED INTO, BY JAVASCRIPT AFTER AJAX CALL **/?>
-						<table border="0" cellpadding="0" cellspacing="0" class="page_system_container">
-						<tr>
-							<td id="logins_prev_td" class="page_system_prev"></td>
-							<td id="logins_page_td" class="page_system_page"></td>
-							<td id="logins_next_td" class="page_system_next"></td>
-						</tr>
-						</table>
-
-					</td>
-				</tr>
+						<td align="right"><?
+							/** PAGE SYSTEM CELLS -- INJECTED INTO, BY JAVASCRIPT AFTER AJAX CALL **/?>
+							<table border="0" cellpadding="0" cellspacing="0" class="page_system_container">
+								<tr>
+									<td id="logins_prev_td" class="page_system_prev"></td>
+									<td id="logins_page_td" class="page_system_page"></td>
+									<td id="logins_next_td" class="page_system_next"></td>
+								</tr>
+							</table></td>
+					</tr>
 				</table>
-
 			</td>
+		</tr>
+		<tr>
+			<td colspan="2">
+				<table border="0" width="100%" id="login_search_table">
+					<tr>
+						<td rowspan="2" width="100" align="center" style="border-right:1px solid #000">
+							<span id="total_count_div"></span>
+						</td>
+						<th class="row2">ID</th>
+						<th class="row2">Username</th>
+						<th class="row2">Result</th>
+						<th class="row2">Section</th>
+						<th class="row2">IP</th>
+						<td><input type="submit" value="Search" name="the_Search_button"></td>
+					</tr>
+					<tr>
+						<td align="center"><input type="text" name="s_id" size="5" value="<?=htmlentities($_REQUEST['s_id'])?>"></td>
+						<td align="center"><input type="text" name="s_username" size="20" value="<?=htmlentities($_REQUEST['s_username'])?>"></td>
+						<td align="center"><select name="s_result">
+								<option value="">[All]</option>
 
+								<option value="success">success</option>
+								<option value="failure">failure</option>
+								<option value="success-code">success-code</option>
+								<option value="failure-code">failure-code</option>
+								<option value="success-api">success-api</option>
+								<option value="failure-api">failure-api</option>
+							</select></td>
+						<td align="center"><select name="s_section">
+								<option value="">[All]</option>
+
+								<option value="admin">admin</option>
+								<option value="client">client</option>
+								<option value="liveclient">liveclient</option>
+								<option value="quiz">quiz</option>
+								<option value="rouster">rouster</option>
+								<option value="roustersys">roustersys</option>
+								<option value="verifier">verifier</option>
+								<option value="API">API</option>
+							</select></td>
+						<td align="center"><input type="text" name="s_ip" size="20" value="<?=htmlentities($_REQUEST['s_ip'])?>"></td>
+						<td><input type="button" value="Reset" onclick="resetLoginForm(this.form);resetPageSystem('<?=$this->index_name?>');loadLogins();"></td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+		</form>
+		<tr>
+			<td colspan="2">
+				<table border="0" width="100%" id="login_table">
+					<tr>
+						<th class="row2" align="left"><?=$this->getOrderLink('id')?>ID</a></th>
+						<th class="row2" align="left"><?=$this->getOrderLink('time')?>Time</a></th>
+						<th class="row2" align="left"><?=$this->getOrderLink('username')?>Username</a></th>
+						<th class="row2" align="left"><?=$this->getOrderLink('result')?>Result</a></th>
+						<th class="row2" align="left"><?=$this->getOrderLink('section')?>Section</a></th>
+						<th class="row2" align="left"><?=$this->getOrderLink('ip')?>IP</a></th>
+					</tr>
+				</table>
+			</td>
 		</tr>
 
-		<tr>
-			<td colspan="2"><table border="0" width="100%" id="login_search_table">
-			<tr>
-				<td rowspan="2" width="100" align="center" style="border-right:1px solid #000">
-
-					<span id="total_count_div"></span>
-
-				</td>
-				<th class="row2">ID</th>
-				<th class="row2">Username</th>
-				<th class="row2">Result</th>
-				<th class="row2">Section</th>
-				<th class="row2">IP</th>
-				<td><input type="submit" value="Search" name="the_Search_button"></td>
-			</tr>
-			<tr>
-				<td align="center"><input type="text" name="s_id" size="5" value="<?=htmlentities($_REQUEST['s_id'])?>"></td>
-				<td align="center"><input type="text" name="s_username" size="20" value="<?=htmlentities($_REQUEST['s_username'])?>"></td>
-				<td align="center"><select name="s_result">
-						<option value="">[All]</option>
-
-						<option value="success">success</option>
-						<option value="failure">failure</option>
-						<option value="success-code">success-code</option>
-						<option value="failure-code">failure-code</option>
-						<option value="success-api">success-api</option>
-						<option value="failure-api">failure-api</option>
-					</select></td>
-				<td align="center"><select name="s_section">
-						<option value="">[All]</option>
-
-						<option value="admin">admin</option>
-						<option value="client">client</option>
-						<option value="liveclient">liveclient</option>
-						<option value="quiz">quiz</option>
-						<option value="rouster">rouster</option>
-						<option value="roustersys">roustersys</option>
-						<option value="verifier">verifier</option>
-						<option value="API">API</option>
-					</select></td>
-				<td align="center"><input type="text" name="s_ip" size="20" value="<?=htmlentities($_REQUEST['s_ip'])?>"></td>
-				<td><input type="button" value="Reset" onclick="resetLoginForm(this.form);resetPageSystem('<?=$this->index_name?>');loadLogins();"></td>
-			</tr>
-			</table></td>
-		</tr></form>
-		<tr>
-			<td colspan="2"><table border="0" width="100%" id="login_table">
-			<tr>
-
-				<th class="row2" align="left"><?=$this->getOrderLink('id')?>ID</a></th>
-				<th class="row2" align="left"><?=$this->getOrderLink('time')?>Time</a></th>
-				<th class="row2" align="left"><?=$this->getOrderLink('username')?>Username</a></th>
-				<th class="row2" align="left"><?=$this->getOrderLink('result')?>Result</a></th>
-				<th class="row2" align="left"><?=$this->getOrderLink('section')?>Section</a></th>
-				<th class="row2" align="left"><?=$this->getOrderLink('ip')?>IP</a></th>
-				<th class="row2">&nbsp;</th>
-			</tr><?
-
-			?></table></td>
-		</tr></table>
+		</table>
 
 		<script>
 
@@ -332,7 +328,56 @@ class LoginTracker{
 
 			loadLogins();
 
-		</script><?
+		</script>
+		<br><br>
+		<table border="5" width="100%">
+			<tr>
+				<td></td>
+				<td align="center">Admin</td>
+				<td align="center">Client</td>
+				<td align="center">Live Client</td>
+				<td align="center">Quiz</td>
+				<td align="center">Rouster</td>
+				<td align="center">Rouster Sys</td>
+				<td align="center">Verifier</td>
+				<td align="center">API</td>
+			</tr>
+			<tr>
+				<td align="right">1 hour</td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>				
+			</tr>
+			<tr>
+				<td align="right">24 hour</td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td align="right">7 day</td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			</table>
+		
+		<?
 
 	}
 
@@ -444,3 +489,4 @@ class LoginTracker{
 		return $var;
 	}
 }
+
