c668ac50691b29e61e175350afbfb3d48e83215a
Jonathan Will <jon@psillc.org>
2019-07-02 11:02:37 -0400

    Attempting ot merge dev branch into my feature

---
+++ api/login_tracker.api.php
@@ -0,0 +1,203 @@
+<?
+
+
+
+class API_LoginTracker{
+
+	var $xml_parent_tagname = "Logins";
+	var $xml_record_tagname = "Login";
+
+	var $json_parent_tagname = "ResultSet";
+	var $json_record_tagname = "Result";
+
+
+	function handleAPI(){
+
+		if(!checkAccess('login_tracker')){
+
+			$_SESSION['api']->errorOut('Access denied to Login Tracker');
+
+			return;
+		}
+
+		switch($_REQUEST['action']){
+		case 'view':
+
+
+			$id = intval($_REQUEST['id']);
+
+			$row = $_SESSION['dbapi']->login_tracker->getByID($id);
+
+			## BUILD XML OUTPUT
+			$out = "<".$this->xml_record_tagname." ";
+
+			foreach($row as $key=>$val){
+
+
+				$out .= $key.'="'.htmlentities($val).'" ';
+
+			}
+
+			$out .= " />\n";
+
+
+			echo $out;
+
+
+
+			break;
+
+		default:
+		case 'list':
+
+			$dat = array();
+			$totalcount = 0;
+			$pagemode = false;
+
+
+			## ID SEARCH
+			if($_REQUEST['s_id']){
+
+				$dat['id'] = intval($_REQUEST['s_id']);
+
+			}
+
+			## USERNAME SEARCH
+			if($_REQUEST['s_username']){
+
+				$dat['username'] = trim($_REQUEST['s_username']);
+
+			}
+
+			## RESULT SEARCH
+			if($_REQUEST['s_result']){
+
+				$dat['result'] = trim($_REQUEST['s_result']);
+
+			}
+
+			## SECTION SEARCH
+			if($_REQUEST['s_section']){
+
+				$dat['section'] = trim($_REQUEST['s_section']);
+
+			}
+			
+			## IP SEARCH
+			if($_REQUEST['s_ip']){
+
+				$dat['ip'] = trim($_REQUEST['s_ip']);
+
+			}			
+
+			## PAGE SIZE / INDEX SYSTEM - OPTIONAL - IF index AND pagesize BOTH PASSED IN
+			if(isset($_REQUEST['index']) && isset($_REQUEST['pagesize'])){
+
+				$pagemode = true;
+
+				$cntdat = $dat;
+				$cntdat['fields'] = 'COUNT(id)';
+				list($totalcount) = mysqli_fetch_row($_SESSION['dbapi']->login_tracker->getResults($cntdat));
+
+				$dat['limit'] = array(
+									"offset"=>intval($_REQUEST['index']),
+									"count"=>intval($_REQUEST['pagesize'])
+								);
+
+			}
+
+
+			## ORDER BY SYSTEM
+			if($_REQUEST['orderby'] && $_REQUEST['orderdir']){
+				$dat['order'] = array($_REQUEST['orderby']=>$_REQUEST['orderdir']);
+			}
+
+
+			$res = $_SESSION['dbapi']->login_tracker->getResults($dat);
+
+
+
+			## OUTPUT FORMAT TOGGLE
+			switch($_SESSION['api']->mode){
+			default:
+
+			## GENERATE XML
+			case 'xml':
+
+				if($pagemode){
+
+					$out = '<'.$this->xml_parent_tagname." totalcount=\"".intval($totalcount)."\">\n";
+				}else{
+					$out = '<'.$this->xml_parent_tagname.">\n";
+				}
+
+				$out .= $_SESSION['api']->renderResultSetXML($this->xml_record_tagname,$res);
+
+				$out .= '</'.$this->xml_parent_tagname.">";
+				break;
+
+			## GENERATE JSON
+			case 'json':
+
+				$out = '['."\n";
+
+				$out .= $_SESSION['api']->renderResultSetJSON($this->json_record_tagname,$res);
+
+				$out .= ']'."\n";
+				break;
+			}
+
+
+			## OUTPUT DATA!
+			echo $out;
+
+		}
+	}
+
+
+
+	function handleSecondaryAjax(){
+
+		$out_stack = array();
+
+
+		foreach($_REQUEST['special_stack'] as $idx => $data){
+
+			$tmparr = preg_split("/:/",$data);
+
+			switch($tmparr[1]){
+			default:
+
+				## ERROR
+				$out_stack[$idx] = -1;
+
+				break;
+			case 'voice_name':
+
+				// COULD BE REPLACED LATER WITH A CUSOMIZABLE SCREEN DB TABLE
+				if($tmparr[2] <= 0){
+					$out_stack[$idx] = '-';
+				}else{
+
+					$out_stack[$idx] = $_SESSION['dbapi']->voices->getName($tmparr[2]);
+
+				}
+
+				break;
+
+			}## END SWITCH
+
+
+
+
+		}
+
+		$out = $_SESSION['api']->renderSecondaryAjaxXML('Data',$out_stack);
+
+		echo $out;
+
+	} ## END HANDLE SECONDARY AJAX
+
+
+}
+
---
+++ dbapi/login_tracker.db.php
@@ -0,0 +1,283 @@
+<?
+/**
+ * Login Tracker SQL Functions
+ */
+
+
+
+class LoginTrackerAPI{
+
+	var $table = "logins";
+
+
+	/**
+	 * Get a Login by ID
+	 * 	@param 	$id		The database ID of the record
+	 * 	 * @return	assoc-array of the database record
+	 */
+	function getByID($id){
+		$id = intval($id);
+
+		return $_SESSION['dbapi']->querySQL("SELECT * FROM `".$this->table."` ".
+						" WHERE id='".$id."' "
+
+					);
+	}
+
+
+	/**
+	 * getResults($asso_array)
+
+	 * Array Fields:
+	 * 	fields	: The select fields for the sql query, * is default
+	 *	id		: Int/Array of Ints
+	 *  enabled	: String only, "yes"/"no"
+	 *
+
+	 *  skip_id : Int/Array of ID's to skip (AND seperated, != operator)
+	 *
+	 *  order : ORDER BY field, Assoc-array,
+	 * 		Example: "order" = array("id"=>"DESC")
+	 *  limit : Assoc-Array of 2 keys/values.
+	 * 		"count"=>(amount to limit by)
+	 * 		"offset"=>(optional, the number of records to skip)
+	 */
+	function getResults($info){
+
+		$fields = ($info['fields'])?$info['fields']:'*';
+
+
+		$sql = "SELECT $fields FROM `".$this->table."` WHERE 1 ";
+
+
+		## ID FIELD SEARCH
+		## ARRAY OF id's SEARCH
+		if(is_array($info['id'])){
+
+			$sql .= " AND (";
+
+			$x=0;
+			foreach($info['id'] as $idx=>$sid){
+				if($x++ > 0)$sql .= " OR ";
+
+				$sql .= "`id`='".intval($sid)."' ";
+			}
+
+			$sql .= ") ";
+
+		## SINGLE ID SEARCH
+		}else if($info['id']){
+
+			$sql .= " AND `id`='".intval($info['id'])."' ";
+
+		}
+
+
+
+		### USERNAME SEARCH
+		## ARRAY OF STRINGS, OR SEPERATED SEARCH
+		if(is_array($info['username'])){
+
+			$sql .= " AND (";
+
+			$x=0;
+			foreach($info['username'] as $idx=>$n){
+				if($x++ > 0)$sql .= " OR ";
+
+				$sql .= "`username` LIKE '%".mysqli_real_escape_string($_SESSION['dbapi']->db,$n)."%' ";
+			}
+
+			$sql .= ") ";
+
+		## SINGLE USERNAME SEARCH
+		}else if($info['username']){
+
+			$sql .= " AND `username` LIKE '%".mysqli_real_escape_string($_SESSION['dbapi']->db,$info['username'])."%' ";
+
+		}
+
+		### RESULT SEARCH
+		## ARRAY OF STRINGS, OR SEPERATED SEARCH
+		if(is_array($info['result'])){
+
+			$sql .= " AND (";
+
+			$x=0;
+			foreach($info['result'] as $idx=>$n){
+				if($x++ > 0)$sql .= " OR ";
+
+				$sql .= "`result` LIKE '%".mysqli_real_escape_string($_SESSION['dbapi']->db,$n)."%' ";
+			}
+
+			$sql .= ") ";
+
+		## SINGLE RESULT SEARCH
+		}else if($info['result']){
+
+			$sql .= " AND `result` LIKE '%".mysqli_real_escape_string($_SESSION['dbapi']->db,$info['result'])."%' ";
+
+		}	
+		
+		### SECTION SEARCH
+		## ARRAY OF STRINGS, OR SEPERATED SEARCH
+		if(is_array($info['section'])){
+
+			$sql .= " AND (";
+
+			$x=0;
+			foreach($info['section'] as $idx=>$n){
+				if($x++ > 0)$sql .= " OR ";
+
+				$sql .= "`section` LIKE '%".mysqli_real_escape_string($_SESSION['dbapi']->db,$n)."%' ";
+			}
+
+			$sql .= ") ";
+
+		## SINGLE SECTION SEARCH
+		}else if($info['section']){
+
+			$sql .= " AND `section` LIKE '%".mysqli_real_escape_string($_SESSION['dbapi']->db,$info['section'])."%' ";
+
+		}
+		
+		### IP SEARCH
+		## ARRAY OF STRINGS, OR SEPERATED SEARCH
+		if(is_array($info['ip'])){
+
+			$sql .= " AND (";
+
+			$x=0;
+			foreach($info['ip'] as $idx=>$n){
+				if($x++ > 0)$sql .= " OR ";
+
+				$sql .= "`ip` LIKE '%".mysqli_real_escape_string($_SESSION['dbapi']->db,$n)."%' ";
+			}
+
+			$sql .= ") ";
+
+		## SINGLE SECTION SEARCH
+		}else if($info['ip']){
+
+			$sql .= " AND `ip` LIKE '%".mysqli_real_escape_string($_SESSION['dbapi']->db,$info['ip'])."%' ";
+
+		}		
+
+
+		## SKIP/IGNORE ID's
+		if(isset($info['skip_id'])){
+
+			$sql .= " AND (";
+
+			if(is_array($info['skip_id'])){
+				$x=0;
+				foreach($info['skip_id'] as $sid){
+
+					if($x++ > 0)$sql .= " AND ";
+
+					$sql .= "`id` != '".intval($sid)."'";
+				}
+
+			}else{
+				$sql .= "`id` != '".intval($info['skip_id'])."' ";
+			}
+
+			$sql .= ")";
+		}
+
+
+		### ORDER BY
+		if(is_array($info['order'])){
+
+			$sql .= " ORDER BY ";
+			$x=0;
+			foreach($info['order'] as $k=>$v){
+				if($x++ > 0)$sql .= ",";
+
+				$sql .= "`$k` ".mysqli_real_escape_string($_SESSION['dbapi']->db,$v)." ";
+			}
+
+		}
+
+		if(is_array($info['limit'])){
+
+			$sql .= " LIMIT ".
+						(($info['limit']['offset'])?$info['limit']['offset'].",":'').
+						$info['limit']['count'];
+
+		}
+
+		## RETURN RESULT SET
+		return $_SESSION['dbapi']->query($sql);
+	}
+
+
+
+	function getCount(){
+
+		$row = mysqli_fetch_row($this->getResults(
+						array(
+							"fields" => "COUNT(id)"
+						)
+					));
+
+		return $row[0];
+	}
+
+	
+	## GET LOGIN TRACKER SECTIONS
+	function getLoginSections(){
+
+		$sql = "SELECT DISTINCT(section) FROM `".$this->table."` WHERE 1";
+
+		$res = $_SESSION['dbapi']->getResult($sql);
+
+		return mysqli_fetch_all($res);
+
+	}
+
+	## GET LOGIN TRACKER DATA AGGREGATION COUNTS
+	function getDataAggrCount($time_range,$section,$result){
+
+		## TIME RANGE CAN BE 1h (1 hour), 24h (24 hour) or 7d (7 days)
+
+		$currtime = time();
+
+		$count_where = "WHERE 1 AND section='".$section."' ";
+
+		if($section=='API' && $result=='success'){
+
+			$count_where .= " AND result='success-api' ";
+
+		}elseif($section=='API' && $result=='failure'){
+
+			$count_where .= " AND result='failure-api' ";
+
+		}else{
+
+			$count_where .= " AND result='".$result."' ";
+
+		}
+
+		if($time_range=='1h'){
+
+			$count_where .= " AND time BETWEEN ".($currtime-3600)." AND ".$currtime." ";
+
+		}elseif($time_range=='24h'){
+
+			$count_where .= " AND time BETWEEN ".($currtime-86400)." AND ".$currtime." ";
+
+		}elseif($time_range=='7d'){
+
+			$count_where .= " AND time BETWEEN ".($currtime-604800)." AND ".$currtime." ";
+
+		}
+
+		$count = $_SESSION['dbapi']->getCount($this->table,$count_where);
+		return $count;
+
+
+
+	}
+
+
+}
