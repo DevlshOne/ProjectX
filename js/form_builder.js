0154369839161a7e18ba36284ea351adea9fdb5a
Dave Mednick <dave@psillc.org>
2019-07-15 13:00:54 -0400

    developing js objects

--- js/form_builder.js
+++ js/form_builder.js
@@ -8,5 +8,39 @@ let _formBuilder = {
     },
     renderTopNav: function() {
 
+    },
+    fbForm: function() {
+
+    },
+};
+function frmField(isRequired, txtLabel, toolTip, placeHolder, cssName, varName, fldValue, fldType, fldMaxLength, fldWidth, fldSpecial, fldOptions, dbTable, dbField, fldVariables, callStep) {
+    this.isRequired = isRequired;
+    this.txtLabel = txtLabel;
+    this.lblWidth = 150;
+    this.lblHeight = 30;
+    this.toolTip = toolTip;
+    this.placeHolder = placeHolder;
+    this.cssName = cssName;
+    this.varName = varName;
+    this.fldValue = fldValue;
+    this.fldType = fldType;
+    this.fldMaxLength = fldMaxLength;
+    this.fldWidth = 200;
+    this.fldHeight = 30;
+    this.fldSpecial = fldSpecial;
+    this.fldOptions = fldOptions;
+    this.dbTable = dbTable;
+    this.dbField = dbField;
+    this.fldVariables = fldVariables;
+    this.callStep = callStep;
+    this.posX = 0;
+    this.posY = 0;
+    this.hideMe = false;
+    this.lockMe = false;
+}
+frmField.prototype = {
+    constructor: frmField,
+    saveToDB: function() {
+
     }
 }
