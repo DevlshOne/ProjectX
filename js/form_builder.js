2c074f22aa3f35e200c2ab1fc9b9fa175449fe03
Dave Mednick <dave@psillc.org>
2019-07-22 23:55:45 -0400

    troubleshooting ajax data load - trying with test data - developing JS 'frmField' class

--- js/form_builder.js
+++ js/form_builder.js
@@ -13,34 +13,46 @@ let _formBuilder = {
 
     },
 };
-function frmField(isRequired, txtLabel, toolTip, placeHolder, cssName, varName, fldValue, fldType, fldMaxLength, fldWidth, fldSpecial, fldOptions, dbTable, dbField, fldVariables, callStep) {
+function frmField(index, isRequired, txtLabel, lblWidth, lblHeight, toolTip, placeHolder, cssName, varName, fldValue, fldType, fldMaxLength, fldWidth, fldHeight, fldSpecial, fldOptions, dbTable, dbField, fldVariables, callStep, fldPosX, fldPosY, lblPosX, lblPosY, isHidden, isLocked) {
     this.isRequired = isRequired;
     this.txtLabel = txtLabel;
-    this.lblWidth = 150;
-    this.lblHeight = 30;
+    this.lblWidth = lblWidth;
+    this.lblHeight = lblHeight;
     this.toolTip = toolTip;
     this.placeHolder = placeHolder;
     this.cssName = cssName;
-    this.varName = varName;
+    this.fldName = varName;
     this.fldValue = fldValue;
     this.fldType = fldType;
     this.fldMaxLength = fldMaxLength;
-    this.fldWidth = 200;
-    this.fldHeight = 30;
+    this.fldWidth = fldWidth;
+    this.fldHeight = fldHeight;
     this.fldSpecial = fldSpecial;
     this.fldOptions = fldOptions;
     this.dbTable = dbTable;
     this.dbField = dbField;
     this.fldVariables = fldVariables;
     this.callStep = callStep;
-    this.posX = 0;
-    this.posY = 0;
-    this.hideMe = false;
-    this.lockMe = false;
+    this.lblPosX = lblPosX;
+    this.lblPosY = lblPosY;
+    this.fldPosX = fldPosX;
+    this.fldPosY = fldPosY;
+    this.isHidden = isHidden;
+    this.isLocked = isLocked;
+    this.index = index;
 }
 frmField.prototype = {
     constructor: frmField,
     saveToDB: function() {
 
+    },
+    populate: function() {
+        let fldRendering = $('#dropZone > li').eq(this.index).find('div.field');
+        for (let property in this) {
+            if (this.hasOwnProperty(property)) {
+                fldRendering.data(property, property.valueOf());
+                console.log('Populating LI #' + this.index + '.' + property + ' = ' + this.property);
+            }
+        }
     }
 }
