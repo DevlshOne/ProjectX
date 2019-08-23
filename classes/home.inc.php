fc7ac28a89b77ad16a87a398a85816487dd2ad7a
Jonathan Will <jon@psillc.org>
2019-08-23 10:32:02 -0400

    Starting to piece together the home screen dashboard idea and structure

--- classes/home.inc.php
+++ classes/home.inc.php
@@ -1,6 +1,35 @@
 <?	/***************************************************************
 	 *	Home class - handles the home interface
 	 *	Written By: Jonathan Will
+	 *
+	 *
+	 *
+	 *
+	 *
+	 *
+	 *
+	 *Preferences Array Structure
+	 *
+	 *"tiles"	This object will hold an array of the tiles that are to be displayed, along with the settings for each tile
+	 *
+	 *'tiles' => array(
+	 *				'type'=>'graph_users_per_hour',
+	 *				'timeframe'=>'week',
+	 *				'size' => 'medium'
+	 *			),
+	 *			array(
+	 *				'type' => 'my_notes',
+	 *			),
+	 *			array(
+	 *				'type' => 'sales_overview',
+	 *				'clusters' => array(23, 25),
+	 *				'user_groups' => array(), // ALL USER GROUPS
+	 *				'timeframe' => 'day'
+	 *			)
+	 *
+	 *
+	 *
+	 *
 	 ***************************************************************/
 
 $_SESSION['home'] = new HomeClass;
@@ -8,9 +37,24 @@ $_SESSION['home'] = new HomeClass;
 
 class HomeClass{
 
+	public $area_name = "home_screen";
+	
+	public $prefs = null;
+	
+	
+	
+	
+	
+	public $orig_prefs = null; // IDEA: USED TO TELL IF PREFERENCES HAVE SAVED SINCE LOADED
+	
+	
+	
 	function HomeClass(){
 
-
+		// LOAD HOME SCREEN PREFERENCES ON INIT
+		$this->prefs = $_SESSION['dbapi']->user_prefs->getData($this->area_name);
+		
+		
 		$this->handlePOST();
 	}
 
@@ -28,6 +72,25 @@ class HomeClass{
 
 	}
 
+	function savePreferences(){
+		
+		return $_SESSION['dbapi']->user_prefs->updateByArray($this->area_name, $this->prefs);
+		
+	}
+	
+	
+	function renderTile($type){
+		
+		
+		switch($type){
+		default:
+			break;
+		
+		}
+		
+	}
+		
+	
 
 	function makeHome(){
 
