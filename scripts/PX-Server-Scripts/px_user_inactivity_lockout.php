8e5c3a766859c2469925085fa70409175f9921b3
Jonathan Will <jon@psillc.org>
2019-10-31 13:06:42 -0400

    Tweeked the inactivity lockout script, to only consider "enabled" users, and made it output a list of the users it disabled

--- scripts/PX-Server-Scripts/px_user_inactivity_lockout.php
+++ scripts/PX-Server-Scripts/px_user_inactivity_lockout.php
@@ -16,8 +16,27 @@
     $user_last_login_field = "last_login";
 
 
+    $where = "WHERE priv >= '".$minimum_priv."' AND ".$user_last_login_field." <= '".intval($inactity_time)."' AND enabled='yes' ";
+    
+    
+    
+    $rows = fetchAllAssoc("SELECT username FROM `users` ".$where." ORDER BY `username` ASC");
+    
+    if(count($rows) > 0){
+	    echo "Disabling the following users: ";
+    	foreach($rows as $row){
+    		echo $row['username']." ";
+	    }
+    	echo "\n";
+    }
+    
+//    exit;
+    
+    
     # BUILD SQL TO UPDATE USERS ENABLED FIELD BASED ON INACTIVITY TIME
-    $sql = "UPDATE `".$users_table."` SET `".$users_enable_field."`='no' WHERE priv >= '".$minimum_priv."' AND ".$user_last_login_field." <= '".intval($inactity_time)."';";
+    $sql = "UPDATE `".$users_table."` SET `".$users_enable_field."`='no' ".$where;
+
+    //echo $sql;exit;
 
     $cnt = execSQL($sql);
 
