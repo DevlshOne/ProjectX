45fb2224ef9d21e6d202edd91b189f65c69ae325
Jonathan Will <jon@psillc.org>
2019-12-13 14:40:12 -0500

    Tweaked User Status report to include new fields

--- classes/user_status_report.inc.php
+++ classes/user_status_report.inc.php
@@ -144,6 +144,8 @@ class UserStatusReport{
 			<th align="left" width="150">Server Name</th>
 			<th align="left" width="150">User Group</th>
 			<th align="left" width="150">Username</th>
+			<th align="left" width="150">Client Mode</th>
+			<th align="left" width="150">Live Mode</th>
 			<th align="left" width="150">Campaign ID</th>
 			<th align="left" width="150">Campaign</th>
 			<th align="left" width="150">Voice</th>
@@ -160,6 +162,21 @@ class UserStatusReport{
 				<td><?=htmlentities($pxuser['server_name'])?></td>
 				<td><?=htmlentities($pxuser['user_group'])?></td>
 				<td><?=htmlentities($pxuser['username'])?></td>
+				<td><?
+				
+					echo htmlentities($pxuser['client_mode']);
+					
+					
+				?></td>
+				<td><?
+				
+					if($pxuser['live_agent_mode'] == "true"){
+						echo "LIVE";
+					}else{
+						echo "System";
+					}
+					
+				?></td>
 				<td><?=htmlentities($pxuser['campaign'])?></td>
 				<td><?=htmlentities($_SESSION['dbapi']->campaigns->getName($pxuser['campaign_id']))?></td>
 				<td><?=htmlentities($_SESSION['dbapi']->voices->getName($pxuser['voice_id']))?></td>
@@ -270,14 +287,14 @@ class UserStatusReport{
 							# GRAB PXUSER ENTRY AND ADD IT TO THE DATA ARRAY
 							$px_user = $_SESSION['JXMLP']->parseOne($pxuser, 'PXUser', 1);
 
-							
+							// ADD THE SERVER ID FIELD TO THE RAW/PARSED ARRAY
 							$px_user['server_name'] = $server['name'];
 							
-							# WE NEED TO CHERRY PICK DATA BECAUSE WE NEED THE SERVER NAME
+							// THEN PUSH WHOLE ARRAY TO THE STACK, SO ADDING NEW FIELDS DOESN'T NEED REMAPPING
 							$user_data[$i] = $px_user;
 							
 							
-							
+							# DAN: WE NEED TO CHERRY PICK DATA BECAUSE WE NEED THE SERVER NAME
 // 							$user_data[$i]['username'] = $px_user['username'];
 // 							$user_data[$i]['campaign'] = $px_user['campaign'];
 // 							$user_data[$i]['campaign_id'] = $px_user['campaign_id'];
