452cdbd7000c515afe3a8c0d0031b03ff419f257
Brent Hansen <bhansen@advancedtci.com>
2020-03-03 09:00:31 -0800

    numbers match lmt

--- dbapi/daily_line_hour.db.php
+++ dbapi/daily_line_hour.db.php
@@ -22,14 +22,19 @@ SELECT
 	call_group,
     sum(agent_paid_sales_cnt) as group_paid_sales_cnt,
     sum(agent_paid_sales_amount) as group_paid_sales_amount,
-    sum(agent_activity_time) as group_activity_time
+    sum(agent_activity_time) as group_activity_time,
+    sec_to_time(sum(agent_activity_time)) as _fat,
+    sum(agent_paid_sales_amount)/(sum(agent_activity_time)/60) as _worked_hour
 FROM ( 
 	SELECT
 		if(RIGHT(username,1) = 2, LEFT(username, length(username) -1), username) as `agent_id`,
+        -- username,
         call_group,
 		sum(paid_sales_cnt) as agent_paid_sales_cnt,
 		sum(paid_sales_amount) as agent_paid_sales_amount,
 		max(activity_time) as agent_activity_time,
+        sec_to_time(max(activity_time)) as _fat,
+        sum(paid_sales_amount)/(max(activity_time)/60) as _worked_hour,
 		count(1) as hands
 		FROM (
 			 SELECT 
@@ -91,8 +96,8 @@ FROM (  SELECT
 			if(RIGHT(username,1) = 2, LEFT(username, length(username) -1), username) as `agent_id`,
 			call_group,
 			count(1) as `hands`,
-			sum(seconds_INCALL+seconds_READY+seconds_QUEUE+seconds_PAUSED)/60 as agent_activity_minutes,
-			(sum(seconds_INCALL+seconds_READY+seconds_QUEUE+seconds_PAUSED)/60)/count(1) as paid_minutes
+			sum(activity_time,)/60 as agent_activity_minutes,
+			(sum(activity_time,)/60)/count(1) as paid_minutes
 		  FROM activity_log
 		WHERE  time_started BETWEEN {$startUnixTime} AND {$endUnixTime}
 		GROUP BY 1) agent_activity
