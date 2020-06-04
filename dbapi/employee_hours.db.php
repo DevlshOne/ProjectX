b4d4fb4f8da42a7e9dd433b7d6ffe0deaa8958d6
Jonathan Will <jon@psillc.org>
2020-06-04 15:50:22 -0400

    Working on employee hours calculation system, rules, scheduling, late detection, manager exemption, 1 hand on time means not late, and other tweeks

--- dbapi/employee_hours.db.php
+++ dbapi/employee_hours.db.php
@@ -9,6 +9,25 @@ class EmployeeHoursAPI{
 
 	var $table = "activity_log";
 
+	var $week_field_arr = null;
+
+	
+	var $shift_flex_time = 300; // IN SECONDS, 5 minutes of flexible time before considered late
+	
+	function __construct() {
+		
+		// THE WEEK ARRAY, THE WEEK ARRAY,THE WEEK ARRAY,THE WEEK ARRAY, IN THE JUNGLE, THE DIGITAL JUNGLE, THE VIRUS SLEEPS TONIIGHHHTTTTT
+		$this->week_field_arr = array();
+		$this->week_field_arr[0] = "work_sun";
+		$this->week_field_arr[1] = "work_mon";
+		$this->week_field_arr[2] = "work_tues";
+		$this->week_field_arr[3] = "work_wed";
+		$this->week_field_arr[4] = "work_thurs";
+		$this->week_field_arr[5] = "work_fri";
+		$this->week_field_arr[6] = "work_sat";
+	}
+	
+	
 
 
 	/**
@@ -269,6 +288,44 @@ class EmployeeHoursAPI{
 	}
 
 
+	function loadSchedules($company_id = 0, $office_id = 0){
+		$out = array();
+		
+		$sql =
+		"SELECT * FROM `schedules` WHERE 1 ".
+		(($company_id > 0)?" AND `company_id`='".intval($company_id)."' ":'').
+		(($office_id > 0)?" AND `office_id`='".intval($office_id)."' ":'').
+		
+		
+		" ORDER BY company_id ASC, office_id ASC, user_group ASC, start_time ASC";
+		
+		//echo $sql;
+		
+		$rowarr = $_SESSION['dbapi']->fetchAllAssoc(
+				$sql
+				);
+		
+		//print_r($rowarr);exit;
+		
+		foreach($rowarr as $row){
+			
+			$cid = intval($row['company_id']);
+			$ofc = intval($row['office_id']);
+			
+			if(!is_array($out[$cid])){
+				$out[$cid] = array();
+			}
+			
+			if(!is_array($out[$cid][$ofc])){
+				$out[$cid][$ofc] = array();
+			}
+			
+			$out[$cid][$ofc][] = $row;
+		}
+		
+		return $out;
+	}
+	
 	function loadRules($company_id = 0){
 		
 		
@@ -276,7 +333,7 @@ class EmployeeHoursAPI{
 		$rowarr = $_SESSION['dbapi']->fetchAllAssoc(
 				
 			"SELECT * FROM `companies_rules` WHERE 1 ".
-			(($company_id > 0)?" AND `company_id`='' ":'').
+			(($company_id > 0)?" AND `company_id`='".intval($company_id)."' ":'').
 			" ORDER BY company_id ASC, trigger_value ASC"
 		);
 	
@@ -286,6 +343,60 @@ class EmployeeHoursAPI{
 		
 		return $out;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	function selectCorrectSchedule($schedules, $time = 0, $user_group = null){
+		
+		if($time == 0)$time = time();
+		
+		$diw = date("w", $time);
+		
+		$fieldname = $this->week_field_arr[$diw];
+
+		$user_group = ($user_group != null)?trim($user_group):null;
+		
+		foreach($schedules as $schedule){
+			
+			
+			// FILTER THE ONES WHO DONT HAVE THE DAY IN QUESTION ENABLED
+			if($schedule[$fieldname] == 'no') continue;
+			
+			
+			// USER GROUP FILTER
+			if($user_group != null){
+				
+				if($schedule['user_group'] == null) continue;
+			
+				$garr = preg_split("/,/", $schedule['user_group'], -1, PREG_SPLIT_NO_EMPTY);
+
+				if(!in_array($user_group , $garr, false )) continue;
+				//if($user_group != $schedule['user_group']) continue;
+			
+				
+				
+			}
+			
+			return $schedule;
+		}
+		
+		
+		// IF NO SCHEDULE FOUND, BUT GROUP WAS SPECIFIED, TRY W/O GROUP
+		if($user_group != null)return $this->selectCorrectSchedule($schedules, $time, null);
+		
+		
+		return null;
+	}
+	
+	
+	
+	
+	
 	
 	function autoCalcEmployeeHours($start_time_override = 0){
 		
@@ -307,11 +418,18 @@ class EmployeeHoursAPI{
 		// LOAD THE RULES DATABASE
 		$all_rules = $this->loadRules();
 		
+		/**
+		 * Schedule Array
+		[ Array (Company ID) ][ Array (Office ID) ] [0-x array of records]
 		
-
+		$all_schedules[0][0] = default global schedules (Group=NULL are globals)
+		**/
+		$all_schedules = $this->loadSchedules();
+		
+		//print_r($all_schedules);
 		
 		// GET STACK OF COMPANYS (and there offices) THAT HAVE AUTO CALCULATE ENABLED
-		$companies = $_SESSION['dbapi']->fetchAllAssoc("SELECT * FROM `companies` WHERE `status`='enabled' AND `opt_auto_calc_hours`='yes' ");
+		$companies = $_SESSION['dbapi']->fetchAllAssoc("SELECT * FROM `companies` WHERE `status`='enabled' AND `opt_auto_calc_hours` IN ('yes','debug-only') ");
 
 		$out .= date("g:i:sa m/d/Y")." - Loading Rules (".count($all_rules).") and Companies (".count($companies).")\n";
 		
@@ -321,6 +439,20 @@ class EmployeeHoursAPI{
 		foreach($companies as $comp){
 			
 			$compname = '#'.$comp['id'].' '.$comp['name'];
+			
+			
+			// (COMPANY SPECIFIC, OFFICE GLOBAL) DEFAULT
+			if(count($all_schedules[$comp['id']][0]) > 0){
+				
+				$cur_schedules = $all_schedules[$comp['id']][0];
+				
+			// (COMPANY AND OFFICE GLOBAL) DEFAULT
+			}else{
+				
+				$cur_schedules = $all_schedules[0][0];
+			}
+			
+			
 			
 			// FOR EACH COMPANY, GRAB ALL EMPLOYEE HOURS RECORDS FOR THEIR OFFICES.
 			$offices = $_SESSION['dbapi']->fetchAllAssoc("SELECT * FROM `offices` WHERE `enabled`='yes' AND `company_id`='".intval($comp['id'])."' ");
@@ -348,7 +480,7 @@ class EmployeeHoursAPI{
 			}
 			
 			
-			$res = $_SESSION['dbapi']->query("SELECT * FROM `activity_log` WHERE `office` IN ($ofcstr) AND `time_started` BETWEEN '$stime' AND '$etime' ");
+			$res = $_SESSION['dbapi']->query("SELECT * FROM `activity_log` WHERE `office` IN ($ofcstr) AND `time_started` BETWEEN '$stime' AND '$etime' ORDER BY `time_started` ASC");
 			
 			$user_activity_arr = array();
 			
@@ -358,6 +490,26 @@ class EmployeeHoursAPI{
 			while($row = mysqli_fetch_array($res, MYSQLI_ASSOC)){
 				
 			//	print_r($row);
+				// (COMPANY SPECIFIC, OFFICE SPECIFIC) DEFAULT
+				if(isset($all_schedules[$comp['id']][$row['office']]) && count($all_schedules[$comp['id']][$row['office']]) > 0){
+					
+					$schedules = $all_schedules[$comp['id']][$row['office']];
+					
+				// (DECIDED ABOVE) DEFAULT
+				}else{
+					
+					$schedules = $cur_schedules;
+					
+				}
+			
+				$schedule = $this->selectCorrectSchedule($schedules, $stime, $row['call_group']);
+				
+				
+
+				
+				
+				//echo "SELECTED SCHEDULE:";
+				//print_r($schedule);exit;
 				
 				$base_username = trim(strtoupper($row['username']));
 				
@@ -367,6 +519,29 @@ class EmployeeHoursAPI{
 					$base_username = substr($base_username,0, strlen($base_username)-1);
 					$second_hand_mode = true;
 				}
+				
+				
+				
+				/**
+				 * CHECK THE ACTIVITY START TIME, VS THE SCHEDULED START TIME, TO DETERMINE IF LATE
+				 */
+				// GENERATE A TIMESTAMP FOR THE START OF THE DAY IN QUESTION
+				$tsttime = mktime(0,0,0, date("m", $stime), date("d", $stime), date("Y", $stime));
+				
+				// ADD TIME OFFSET FROM SCHEDULE
+				$tsttime+= $schedule['start_time'];
+				
+				$hand_is_late = false;
+				
+				if($row['time_started'] > ($tsttime + $this->shift_flex_time) ){
+					
+					$out .=  "USER IS LATE: $base_username started(".date("H:i:s", $row['time_started']).") vs scheduled(".date("H:i:s", ($tsttime + $this->shift_flex_time)).")\n";
+					$hand_is_late = true;
+				}else{
+					//$out .=  "USER ON TIME: $base_username started(".date("H:i:s", $row['time_started']).") vs scheduled(".date("H:i:s", ($tsttime + $this->shift_flex_time)).")\n";
+				}
+				
+				
 				
 				
 				//echo "BASE USERNAME: $base_username \n"; /// + $row['seconds_DISPO'] 
@@ -390,6 +565,8 @@ class EmployeeHoursAPI{
 				
 					$user_activity_arr[$base_username]['num_hands'] = count($user_activity_arr[$base_username]['activity']);
 					
+					$user_activity_arr[$base_username]['num_hands_late'] += (($hand_is_late)?1:0);
+					
 					if(!$second_hand_mode){
 						$user_activity_arr[$base_username]['main_user_activity_id'] = $row['id'];
 					}
@@ -403,10 +580,12 @@ class EmployeeHoursAPI{
 						'main_user_activity_id' => $row['id'],
 						'activity_time'		=> $activity_time,
 						'activity'			=> array($row),
-						'num_hands'			=> 1
+						'schedule'			=> $schedule,
+						'num_hands'			=> 1,
+						'num_hands_late'	=> ($hand_is_late)?1:0
 					);
 				}
-				
+
 			}
 			
 			
@@ -419,13 +598,82 @@ class EmployeeHoursAPI{
 				
 				$rule_breaker = false;
 				
+				$schedule = $row['schedule'];
+				
+				/**
+				 * SCHEDULE - DETECT IF THE USER STARTED LATE, COMPARED TO THE SCHEDULE
+				 */
+				$is_user_late = ($row['num_hands_late'] == $row['num_hands'])?'yes':'no';
+				
+				// IF THE USER IS ABOUT TO BE MARKED AS LATE, PRE-SET TEH CURRENT DATE TO LATE TOO, SO IT CALCULATES RIGHT
+				if($row['activity'][0]['has_set_late'] == 'no'){
+	
+					$row['activity'][0]['is_late'] = $is_user_late;
+					
+				}
+						
+						
+
+				
+
+				
+				
+				$applicable_rules = array();
+				
+				// PRE-SCAN AND FILTER RULES
 				foreach($rules as $rule){
-					
-					if($rule_breaker)break;
-					
+	
 					if( ($rule['late_rule'] == 'yes' && $row['activity'][0]['is_late'] != 'yes') ||
 						($rule['late_rule'] == 'no' && $row['activity'][0]['is_late'] == 'yes')
 					){ continue;}
+					
+					/**
+					 * SCHEDULE - DETECT WHICH SET OF RULES TO USE, BASED ON SCHEDULE
+					 */
+					if($schedule['id'] && $rule['schedule_id'] != $schedule['id']){
+						continue;
+					}else if(!$schedule['id'] && $rule['schedule_id'] != 0){
+						continue;
+					}
+
+					$applicable_rules[] = $rule;
+				}
+				
+				// SECOND PASS, FOR GLOBAL SCHEDULE RULES
+				if(count($applicable_rules) <= 0){
+					
+					$applicable_rules = array();
+					
+					// SECOND PRE-SCAN AND FILTER OF RULES
+					foreach($rules as $rule){
+						
+						if( ($rule['late_rule'] == 'yes' && $row['activity'][0]['is_late'] != 'yes') ||
+							($rule['late_rule'] == 'no' && $row['activity'][0]['is_late'] == 'yes')
+						){ continue;}
+						
+						if($rule['schedule_id'] != 0){
+							continue;
+						}
+						
+						$applicable_rules[] = $rule;
+					}
+				}
+				
+				
+				echo "Agent ".$row['user']." - Applicable rules: ".	print_r($applicable_rules, 1)."\n";
+				
+
+				
+			//	echo "Schedule: ".print_r($row,1)."\n";
+				
+				foreach($applicable_rules as $rule){
+					
+					if($rule_breaker)break;
+					
+// 					if( ($rule['late_rule'] == 'yes' && $row['activity'][0]['is_late'] != 'yes') ||
+// 						($rule['late_rule'] == 'no' && $row['activity'][0]['is_late'] == 'yes')
+// 					){ continue;}
+
 					
 					
 					
@@ -496,7 +744,7 @@ class EmployeeHoursAPI{
 				// UPDATE THE EMPLOYEES ACTIVITY LOG RECORD
 				if($activity_time != $paid_hrs){
 
-					// UPPER CAP HIT
+					// UPPER CAP HIT                
 					if($rule_breaker){
 						
 						$out .= date("g:i:sa m/d/Y")." ".$compname." - User $base_username (".$row['activity'][0]['call_group'].")".(($row['num_hands'] > 1)?'(Hands: '.$row['num_hands'].')':'').": Updating Paid Time to $paid_hrs\t\t($activity_time Capped)\n";
@@ -509,9 +757,36 @@ class EmployeeHoursAPI{
 					$out .= date("g:i:sa m/d/Y")." ".$compname." - User $base_username (".$row['activity'][0]['call_group'].")".(($row['num_hands'] > 1)?'(Hands: '.$row['num_hands'].')':'').": Updating Paid Time to $paid_hrs\n";
 				}
 
-				// UPDATE ACTIVITY LOG RECORD FOR MAIN USER 
-				$_SESSION['dbapi']->execSQL("UPDATE `activity_log` SET `paid_time`='".(($paid_hrs * 60))."', `paid_break_time`='".($users_break_time * 60)."' WHERE `id`='".$row['main_user_activity_id']."' ");
 				
+				
+				
+				
+				$extrasql = '';
+				if($row['activity'][0]['has_set_late'] == 'no'){
+				
+					$extrasql .= ",is_late='".$is_user_late."',has_set_late='yes-$is_user_late' ";
+
+				}
+				
+				$sql = "UPDATE `activity_log` SET `paid_time`='".(($paid_hrs * 60))."', `paid_break_time`='".($users_break_time * 60)."' $extrasql WHERE `id`='".$row['main_user_activity_id']."' ";
+				
+				
+				
+				if($comp['opt_auto_calc_hours'] == 'yes'){
+				
+					//echo $sql."\n";
+					// UPDATE ACTIVITY LOG RECORD FOR MAIN USER 
+					$_SESSION['dbapi']->execSQL($sql);
+					
+				}else{
+					echo "Company (#".$comp['id'].' - '.$comp['name'].") in Debug only mode, no changes made.\n";
+					
+				}
+
+				
+				
+				
+				//echo "TEETH DISABLED - No changes made\n";
 				
 				
 				
